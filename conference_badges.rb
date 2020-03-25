3965192cd6be9f117770a580350cf07daa0a7381
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-03-25 23:37:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(arg)
+  p "Hello, my name is #{arg}."
+end
+
+def batch_badge_creator(arg)
+  arr = []
+  arg.each {|el| arr << badge_maker(el)}
+  arr
+end
+
+def assign_rooms(arg)
+  arg.each_with_index.map{|el, index| p "Hello, #{el}! You'll be assigned to room #{index +1}!"}
+end
+
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+end
